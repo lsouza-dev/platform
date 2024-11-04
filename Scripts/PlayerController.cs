610a533c16acf7b86075502f22d11bc47ac1a0f8
Luiz Fabiano de Souza <LuizFabianoDS04@gmail.com>
2024-11-04 14:42:22 -0300

    Double Jump

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,8 +15,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float runSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private bool isGrounded;
+    [SerializeField] private bool canDoubleJump;
     [SerializeField] private Transform groundCheckPoint;
     [SerializeField] private float groundCheckRadius;
+    // Mascara de layer que será usada para verificar a colisão
+    // entre os objetos
     [SerializeField] private LayerMask whatIsGround;
 
     private void Awake()
@@ -30,6 +34,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        // Se o raio do groundCheckpoint do player estiver colidindo com a 
+        // layer ground, a variável receberá true
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
         float xInput = Input.GetAxis("Horizontal");
 
         activeSpeed = moveSpeed;
@@ -39,8 +48,31 @@ public class PlayerController : MonoBehaviour
         
         
         bool yInput = Input.GetButtonDown("Jump");
-        if(yInput) rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
+        if(yInput)
+        {
+            // Se está no chão
+            if (isGrounded)
+            {
+                // Pula e habilita doubleJump
+                Jump();
+                canDoubleJump = true;
+            }
+            else
+            {
+                // Se puder usar o double jump
+                if (canDoubleJump)
+                {
+                    // Pula novamente e desativa o double jump
+                    Jump();
+                    canDoubleJump= false;
+                }
+            }
+        }
 
     }
 
+    private void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
 }
